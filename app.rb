a5a092912316e5df9d472184365798007f1f7de1
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-26 22:06:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
 
   post '/checkout' do
     "ddddddddddddd"
-    #{params}
+    "#{params}"  
   # session[:item] = params[:item]
   #  @session[:item] = params[:item]
   #  erb :checkout
